4f5cbf40a4741385bad1ae0e7b373ff8b7edcaf6
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2017-10-14 13:07:29 -0400

    removed .next, updated gitignore

--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -14,42 +14,6 @@ var _link2 = _interopRequireDefault(_link);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _jsxFileName = '/Users/ricardoplouis/Desktop/dev/custom-server-express/pages/index.js?entry';
-
 exports.default = function () {
-  return _react2.default.createElement('ul', {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 5
-    }
-  }, _react2.default.createElement('li', {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 6
-    }
-  }, _react2.default.createElement(_link2.default, { href: '/b', as: '/a', __source: {
-      fileName: _jsxFileName,
-      lineNumber: 6
-    }
-  }, _react2.default.createElement('a', {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 6
-    }
-  }, 'a'))), _react2.default.createElement('li', {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 7
-    }
-  }, _react2.default.createElement(_link2.default, { href: '/a', as: '/b', __source: {
-      fileName: _jsxFileName,
-      lineNumber: 7
-    }
-  }, _react2.default.createElement('a', {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 7
-    }
-  }, 'b'))));
+  return _react2.default.createElement('ul', null, _react2.default.createElement('li', null, _react2.default.createElement(_link2.default, { href: '/b', as: '/a' }, _react2.default.createElement('a', null, 'a'))), _react2.default.createElement('li', null, _react2.default.createElement(_link2.default, { href: '/a', as: '/b' }, _react2.default.createElement('a', null, 'b'))));
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiTGluayJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQU8sQUFFUDs7Ozs7Ozs7a0JBQWUsWUFBQTt5QkFDYixjQUFBOztnQkFBQTtrQkFBQSxBQUNFO0FBREY7QUFBQSxHQUFBLGtCQUNFLGNBQUE7O2dCQUFBO2tCQUFBLEFBQUk7QUFBSjtBQUFBLHFCQUFJLEFBQUMsZ0NBQUssTUFBTixBQUFXLE1BQUssSUFBaEIsQUFBbUI7Z0JBQW5CO2tCQUFBLEFBQXdCO0FBQXhCO3FCQUF3QixjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsS0FEOUIsQUFDRSxBQUFJLEFBQXdCLEFBQzVCLHdCQUFBLGNBQUE7O2dCQUFBO2tCQUFBLEFBQUk7QUFBSjtBQUFBLHFCQUFJLEFBQUMsZ0NBQUssTUFBTixBQUFXLE1BQUssSUFBaEIsQUFBbUI7Z0JBQW5CO2tCQUFBLEFBQXdCO0FBQXhCO3FCQUF3QixjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsS0FIakIsQUFDYixBQUVFLEFBQUksQUFBd0I7QUFIaEMiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL3JpY2FyZG9wbG91aXMvRGVza3RvcC9kZXYvY3VzdG9tLXNlcnZlci1leHByZXNzIn0=
