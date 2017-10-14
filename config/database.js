f0df80020eb8d565867536541a168bde112b7b56
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2017-10-14 15:02:15 -0400

    added mongo uri env var

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 //  url : 'mongodb://ricardoplouis:47By23iS@ds053320.mlab.com:53320/learnmern'
-  url :'mongodb://ricardoplouis:47By23iS@ds153113.mlab.com:53113/meetingminutes',
+  url : process.env.MONGO_URI || 'mongodb://ricardoplouis:47By23iS@ds153113.mlab.com:53113/meetingminutes',
 }
